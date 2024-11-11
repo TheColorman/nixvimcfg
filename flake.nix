f011d6fc4fa394431704884a00f41b3ccfa6d457
TheColorman <github@colorman.me>
2024-11-11 14:48:50 +1100

    Update

--- flake.nix
+++ flake.nix
@@ -2,17 +2,33 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # nixpkgs is only included so I can control the version used by nixvimchad
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixvimchad.url = "github:thecolorman/nixvimchad";
     nixvimchad.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {nixvimchad, ...}:
-    nixvimchad.configure ({pkgs,...}: {
-      nixvimConfig = (import ./nixvim.nix) // {
-        extraPackages = with pkgs; [ nixd ];
-      };
+  outputs = {nixvimchad, self, ...}:
+    nixvimchad.configure ({pkgs,...}:  (import ./nixvim.nix) // {
+      extraPackages = with pkgs; [ nixd ];
 
-      lspconfig.servers = [ "nixd" ];
+      chad.plugins.lspconfig.pluginConfig.config = ''
+        function()
+          require("nvchad.configs.lspconfig").defaults()
+          
+          local lspconfig = require "lspconfig"
+          
+          local servers = { "nixd" }
+          local nvlsp = require "nvchad.configs.lspconfig"
+          
+          for _, lsp in ipairs(servers) do
+            lspconfig[lsp].setup {
+              on_attach = nvlsp.on_attach,
+              on_init = nvlsp.on_init,
+              capabilities = nvlsp.capabilities,
+            }
+          end
+        end
+      '';
     });
   }
