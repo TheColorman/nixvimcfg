276104cf1fab915167fe9c121a4da4e959e36427
TheColorman <github@colorman.me>
2025-02-13 11:48:13 +0100

    Add cland

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
 
             local lspconfig = require "lspconfig"
 
-            local servers = { "nixd", "svelte" }
+            local servers = { "nixd", "svelte", "clangd" }
             local nvlsp = require "nvchad.configs.lspconfig"
 
             for _, lsp in ipairs(servers) do
