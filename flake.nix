87000d31246c7d0133ba2ef2cb0aa16bd9fe121f
TheColorman <colorman@github.com>
2025-02-27 01:16:53 +1100

    Add config to roslyn and csharpier

--- flake.nix
+++ flake.nix
@@ -28,7 +28,60 @@
           pkg = roslyn-nvim;
           ft = "cs";
           opts.__raw = ''{
-            config = {},
+            config = {
+              on_init = function(client)
+                  -- client.offset_encoding = "utf-8"
+              end,
+              handlers = {
+                  ["textDocument/hover"] = function(err, result, ctx, config)
+                      if result and result.contents and result.contents.value then
+                          result.contents.value = result.contents.value:gsub("\\([^%w])", "%1")
+                      end
+                      vim.lsp.handlers["textDocument/hover"](err, result, ctx, config)
+                  end,
+              },
+              on_attach = function(client, bufnr)
+                  vim.api.nvim_set_keymap(
+                      "n",
+                      "gd",
+                      "<cmd>lua vim.lsp.buf.definition()<CR>",
+                      { noremap = true, silent = true }
+                  )
+                  vim.api.nvim_set_keymap(
+                      "n",
+                      "gD",
+                      "<cmd>lua vim.lsp.buf.declaration()<CR>",
+                      { noremap = true, silent = true }
+                  )
+                  vim.api.nvim_set_keymap(
+                      "n",
+                      "gi",
+                      "<cmd>lua vim.lsp.buf.implementation()<CR>",
+                      { noremap = true, silent = true }
+                  )
+              end,
+              settings = {
+                  ["csharp|inlay_hints"] = {
+                      csharp_enable_inlay_hints_for_implicit_object_creation = true,
+                      csharp_enable_inlay_hints_for_implicit_variable_types = true,
+                      csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+                      csharp_enable_inlay_hints_for_types = true,
+                      dotnet_enable_inlay_hints_for_indexer_parameters = true,
+                      dotnet_enable_inlay_hints_for_literal_parameters = true,
+                      dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+                      dotnet_enable_inlay_hints_for_other_parameters = true,
+                      dotnet_enable_inlay_hints_for_parameters = true,
+                      dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = false,
+                      dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = false,
+                      dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = false,
+                  },
+                  -- ["csharp|background_analysis"] = {
+                  --     dotnet_compiler_diagnostics_scope = "fullSolution",
+                  --     dotnet_analyzer_diagnostics_scope = "fullSolution",
+                  -- },
+                  ["csharp|code_lens"] = { dotnet_enable_references_code_lens = true },
+              },
+            },
             exe = "Microsoft.CodeAnalysis.LanguageServer",
             args = {
                 "--logLevel=Information", "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path())
@@ -68,12 +121,32 @@
             }
           end
         '';
-        conform.pluginConfig.opts = {
-          formatters_by_ft = {lua = ["stylua"];};
-          format_on_save = {
-            timeout_ms = 500;
-            lsp_fallback = true;
+        conform.pluginConfig = {
+          opts = {
+            formatters_by_ft = {
+              lua = ["stylua"];
+              cs = ["csharpier"];
+              css = ["prettier"];
+              html = ["prettier"];
+              vue = ["prettier"];
+              typescript = ["prettier"];
+            };
+            format_after_save = {
+              timeout_ms = 500;
+              lsp_fallback = true;
+            };
           };
+          config.__raw = ''
+            function(_, opts)
+              local conform = require("conform")
+
+              conform.setup(opts)
+
+              conform.formatters.csharpier = {
+                args = { "--write-stdout", "--no-cache", "$FILENAME" },
+              }
+            end
+          '';
         };
 
         cmp.pluginConfig.opts.__raw = ''
